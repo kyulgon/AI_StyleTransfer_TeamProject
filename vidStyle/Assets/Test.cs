3d8a49ac517ad19ab310c3f2aa60dbdf7b04cc2b
kyulgon <57319096+kyulgon@users.noreply.github.com>
2021-05-11 17:37:50 +0900

    Portal 생성 Img, Video 보여주기

---
+++ vidStyle/Assets/Test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video; // 비디오플레이어 사용
+
+public class Test : MonoBehaviour
+{
+    public int aa;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ vidStyle/Assets/VideoPlayerTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoPlayerTest : MonoBehaviour
+{
+    public VideoClip[] videoClip;
+    private VideoPlayer vp;
+    private int currentClipIndex = 0;
+
+    private void Start()
+    {
+        //vp = GetComponent<VideoPlayer>();
+        //vp.loopPointReached += OnMovieFinished;
+        vp.clip = videoClip[currentClipIndex];
+
+        vp.playOnAwake = false;
+        vp.isLooping = false;
+
+        vp.Pause();
+    }
+
+    private void Update()
+    {
+        Debug.Log(vp.targetCameraAlpha);
+
+    }
+}
